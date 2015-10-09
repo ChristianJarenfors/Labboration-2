ad260f5fdfee79d299d29f27eb4a153f121cd557
ChrilleJ <christian.jarenfors@gmail.com>
2015-10-09 13:27:19 +0200

    Startup of Project

---
+++ Labboration2ChristianJ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labboration2ChristianJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
