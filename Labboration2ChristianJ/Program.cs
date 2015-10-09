247b42212e4de76cf4ce99265944fcd1b9c1afdb
ChrilleJ <christian.jarenfors@gmail.com>
2015-10-09 14:24:51 +0200

    Uppgift2.1 tillagd

--- Labboration2ChristianJ/Program.cs
+++ Labboration2ChristianJ/Program.cs
@@ -8,29 +8,33 @@ namespace Labboration2ChristianJ
 {
     class Program
     {
+        public static int value = 0;
+          
         static void Main(string[] args)
         {
             //Uppgift 1
             Uppgift1_1_WaterBoil();
             Uppgift1_2_PekkasPonnys();
             Uppgift1_3_GårDetAttBada();
+            Uppgift2_2_FizzBuzz();
             Console.ReadKey();
         }
 
         
+
+
         #region Uppgift 1.1 Metoder
 
         private static void Uppgift1_1_WaterBoil()
         {
             Console.Write("Hur varmt är vattnet? ");
-            int temp = 0;
-            int.TryParse(Console.ReadLine(), out temp);
-            doesTheWaterBoil(temp);
+            int.TryParse(Console.ReadLine(), out value);
+            doesTheWaterBoil(value);
         }
 
         private static void doesTheWaterBoil(int temp)
         {
-            if(temp==100)
+            if(temp>=100)
             {
                 Console.WriteLine("Det kokar!!!");
             }else if(temp>=50)
@@ -47,7 +51,6 @@ namespace Labboration2ChristianJ
         #region Uppgift 1.2 Metoder
         private static void Uppgift1_2_PekkasPonnys()
         {
-            int value = 0;
             Console.Write("Hur gammal är du? ");
             int.TryParse(Console.ReadLine(), out value);
             if (value<12)
@@ -86,7 +89,48 @@ namespace Labboration2ChristianJ
         #region Uppgift 1.3 Metoder
         private static void Uppgift1_3_GårDetAttBada()
         {
-            throw new NotImplementedException();
+
+            Console.Write("Finns det vatten? (ja/nej): ");
+            if(Console.ReadLine()=="ja")
+            {
+                Console.Write("Vilken temperatur har vattnet? ");
+                int.TryParse(Console.ReadLine(), out value);
+                if (value>=30)
+                {
+                    Console.WriteLine("Varsågod att bada.");
+                }
+                else
+                {
+                    Console.WriteLine("Det går inte att bada. (Vattnet är för kallt)");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Det går inte att bada. (Inget vatten)");
+            }
+
+        }
+        #endregion
+
+        #region Uppgift 2.1 Metoder
+        private static void Uppgift2_2_FizzBuzz()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i%3==0||i%5==0)
+                {
+                    Console.Write(i + ". ");
+                    if (i % 3==0)
+                    {
+                        Console.Write("Fizz");
+                    }
+                    if (i%5==0)
+                    {
+                        Console.Write("Buzz");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
         #endregion
     }
